24e6d9342637220d15d921b15c5f9bd40afd49aa
TobeSoftwareGmbH <tobias.kamm@pm.me>
2019-02-09 14:13:54 +0100

    Kommentare hinzugefügt

--- js_tools/slideshow.js
+++ js_tools/slideshow.js
@@ -10,10 +10,10 @@ function showSlide() {
   if(page > maxPage) page = 1;
   if(page < 1) page = maxPage;
 
-  document.getElementById("number_indicator").innerHTML = page + " / " + maxPage;
-  document.getElementById("slideshow_img").src = "img/slideshow/slideshow_" + page + ".jpg";
+  document.getElementById("number_indicator").innerHTML = page + " / " + maxPage; //Seitennummer wird aktualisiert
+  document.getElementById("slideshow_img").src = "img/slideshow/slideshow_" + page + ".jpg"; //Bild wird geändert (Muster für Dateiname: slideshow_#.jpg)
 
-  switch(page) {
+  switch(page) { //Je nach Seite eine andere Beschreibung
     case 1:
       document.getElementById("img_heading_1").innerHTML = "Zweiter Stern für uns!";
       document.getElementById("img_heading_2").innerHTML = "Bild der Verleihung vom 22. Oktober 2018";
