8f68587a0c8fa9e4ce7299e021c2b26ca6389abf
TobeSoftwareGmbH <tobias.kamm@pm.me>
2019-02-10 21:17:58 +0100

    JS-Gerüst erstellt

--- js_tools/eco_week.js
+++ js_tools/eco_week.js
@@ -1,1 +1,11 @@
+function getContent(class_level) {
+  var request = new XMLHttpRequest();
+   request.open("GET", "../../JSON/eco_week.json", false);
+   request.send(null);
 
+
+  var json = request.responseText;
+  var json_object = JSON.parse(json);
+
+  console.log(json_object);
+}
