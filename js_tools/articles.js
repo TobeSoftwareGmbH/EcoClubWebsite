25585feaed36f2c6d55ba82df064d083ba184830
mariosbf <mariosbf@hotmail.com>
2019-03-10 17:54:56 +0100

    Code auf Umstrukturierung angepasst.

--- js_tools/articles.js
+++ js_tools/articles.js
@@ -5,42 +5,41 @@ function displayArticlePreview() {
   var output = "";
   var json_object = JSON.parse(readTextFile("../../JSON/articles.json"));
 
-  console.log(json_object.articles);
+  //console.log(json_object.articles);
   for(var art = 0; art < json_object.articles.length; art++) { //Alle Artikel durchgehen
     articleObject = json_object.articles[art];
 
-    title = articleObject[0];
-    author = "von " + articleObject[1];
-    description = articleObject[2];
-    image_file = (articleObject[3] == "placeholder") ? "placeholder.png" : articleObject[3];
-    file = articleObject[4];
-    pub_date = "veröffentlicht am " + articleObject[5]; //not used
+    title = articleObject.title;
+    author = "von " + articleObject.author;
+    description = articleObject.description;
+    image_file = (articleObject.picture == "placeholder") ? "placeholder.png" : articleObject[3];
+    pub_date = "veröffentlicht am " + articleObject.pub_date //not used
 
     output += "<div class='article'><a href='article.php?id=" + art +"'><img class='art_img' src='../../img/articles_preview/"+image_file+"'></img>"
-    output += "<div class='art_info'><h4 class='art_title'>"+title+"</h4><p class='art_author'>"+author+"</p><p class='art_description'>"+description+"</p></div>"
+    output += "<div class='art_info'><h4 class='art_title'>" + title + "</h4><p class='art_author'>"+author+"</p><p class='art_description'>"+description+"</p></div>"
     output += "</a></div>"
     output += " <div style='clear: both;'></div> "
   }
 
-  console.log(output);
+  //console.log(output);
   document.getElementById("article-list").innerHTML = output; //output dem dafür vorgesehenem div hinzufügen
 }
 
 function displayArticle(id) {
-  //console.log(id);
+  console.log(id);
   var title, author, description, output, paragraphs;
 
   var json_object = JSON.parse(readTextFile("../../JSON/articles.json"));
   var articleObject = json_object.articles[id];
 
-  console.log(json_object);
+  //console.log(json_object);
 
-  title = articleObject[0];
-  author = articleObject[1];
-  description = articleObject[2];
+  title = articleObject.title;
+  author = articleObject.author;
+  description = articleObject.description;
 
-  paragraphs = articleObject[6];
-  console.log(articleObject);
+  paragraphs = articleObject.paragraphs;
+  //console.log(articleObject);
 
   for(var i = 0; i < paragraphs.length; i++) {
     document.getElementById("article_text").innerHTML += "<p>" + paragraphs[i] + "</p>";
