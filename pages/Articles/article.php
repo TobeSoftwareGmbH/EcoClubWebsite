562d3914e5c7aa6f17e01d9ae14fadc2acb1fcad
Mario Furtado <mariosbf@hotmail.com>
2019-03-10 16:12:14 +0100

    Artikel ID eingefügt.

---
+++ pages/Articles/article.php
@@ -0,0 +1,22 @@
+<html>
+  <head>
+    <title>Artikel - Der Eco-Club an der ESM</title>
+    <link rel="stylesheet" type="text/css" href="../../css_tools/general_style.css">
+    <link rel="stylesheet" type="text/css" href="../../css_tools/article_list.css">
+    <link rel="stylesheet" type="text/css" href="../../css_tools/animate.css">
+    <script type="text/javascript" src="../../js_tools/articles.js"></script>
+    <meta charset="utf-8">
+  </head>
+
+  <body>
+    <object class="menu animated fadeIn" type="text/html" data="../../html_tools/menu.html"></object>
+
+    <div class="content animated fadeIn">
+    </div>
+
+    <br><br><br><br>
+
+    <object class="bottom animated fadeIn" type="text/html" data="../../html_tools/bottom.html"></object>
+  </body>
+
+</html>
