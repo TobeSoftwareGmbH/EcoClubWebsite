244d6e96137da7942ad715cf135a92a84177835f
mariosbf <mariosbf@hotmail.com>
2019-03-16 21:46:23 +0100

    Ich hoffe ich habe nix geschrottet.

--- js_tools/lang_tools.js
+++ js_tools/lang_tools.js
@@ -1,10 +1,16 @@
 function getLang () {
-  var userLang = navigator.language || navigator.userLanguage;
-  var lang;
+  var cookie = retrieve_cookie("lang");
 
-  if(userLang.includes("en")) lang = "en";
-  else if(userLang.includes("de")) lang = "de";
-  else lang = "en";
+  if(cookie === "") {
+    var userLang = navigator.language || navigator.userLanguage;
+    var lang;
+
+    if(userLang.includes("en")) lang = "en";
+    else if(userLang.includes("de")) lang = "de";
+    else lang = "en";
+  } else {
+    lang = cookie;
+  }
 
   return lang;
 }
@@ -147,8 +153,90 @@ function readTextFile(file) {
 }
 
 function changeLanguage() {
-  console.log("Language changed");
+  var languages = JSON.parse(readTextFile("../JSON/languages.json"));
+  var div = document.getElementById('lang');
+  var li, a;
 
-  var fs = require('fs');
-  var files = fs.readdirSync('/assets/photos/');
+  console.log("Language change");
+  console.log(languages);
+
+  div.removeChild(document.getElementById('languageButton'));
+
+  for (var i = 0; i < languages.length; i++) {
+    li = document.createElement("li");
+    a = document.createElement("a");
+
+    li.className = "menu_item item_right item_lang menu_link";
+
+    a.className = "menu_link";
+    a.setAttribute("onclick", "setLanguage('" + languages[i] + "')");
+    a.innerHTML = languages[i].toUpperCase();
+
+    div.appendChild(li);
+    li.appendChild(a);
+  }
+}
+
+function setLanguage(lang) {
+  var current;
+
+  current = retrieve_cookie("lang");
+  //console.log(current);
+
+  if (!(current === "")) {
+    delete_cookie("lang");
+  } else {
+    console.log("Cookie not set")
+  }
+
+  create_cookie("lang", lang, 1, "/");
+
+  window.top.location.reload();
+}
+/* from https://www.pontikis.net/blog/create-cookies-php-javascript
+ * Create cookie with javascript
+ *
+ * @param {string} name cookie name
+ * @param {string} value cookie value
+ * @param {int} days2expire
+ * @param {string} path
+ */
+function create_cookie(name, value, days2expire, path) {
+  var date = new Date();
+  date.setTime(date.getTime() + (days2expire * 24 * 60 * 60 * 1000));
+  var expires = date.toUTCString();
+  document.cookie = name + '=' + value + ';' +
+                   'expires=' + expires + ';' +
+                   'path=' + path + ';';
+}
+
+/*From https://www.pontikis.net/blog/create-cookies-php-javascript
+ * Retrieve cookie with javascript
+ *
+ * @param {string} name cookie name
+ */
+function retrieve_cookie(name) {
+  var cookie_value = "",
+    current_cookie = "",
+    name_expr = name + "=",
+    all_cookies = document.cookie.split(';'),
+    n = all_cookies.length;
+
+  for(var i = 0; i < n; i++) {
+    current_cookie = all_cookies[i].trim();
+    if(current_cookie.indexOf(name_expr) == 0) {
+      cookie_value = current_cookie.substring(name_expr.length, current_cookie.length);
+      break;
+    }
+  }
+  return cookie_value;
+}
+
+/*from https://www.pontikis.net/blog/create-cookies-php-javascript
+ * Delete cookie with javascript
+ *
+ * @param {string} name cookie name
+ */
+function delete_cookie(name) {
+  document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
 }
